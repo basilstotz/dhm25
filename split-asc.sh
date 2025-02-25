8eb2b32090178b90fe58c62e04ce9a47a02e77b4
Basil Stotz <stotz.basil@opinsys.fi>
2025-02-25 09:44:34 +0100

    mv lines to root

--- split-asc.sh
+++ split-asc.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-mkdir -p ASCII_GRID_1part/lines
+mkdir -p lines
 LINE=1;
 while read -r x;do
     NAME=$( printf "%06i" $LINE )
-    echo "$x" > ASCII_GRID_1part/lines/$NAME.txt
+    echo "$x" > lines/$NAME.txt
     LINE=$(( LINE + 1 ))
 done < ASCII_GRID_1part/dhm25_grid_raster.asc
 
