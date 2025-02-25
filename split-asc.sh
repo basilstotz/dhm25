b34af860c0e9e948e2867fa0320096ec82b1fba8
Basil Stotz <stotz.basil@opinsys.fi>
2025-02-25 09:40:04 +0100

    fix paths

--- split-asc.sh
+++ split-asc.sh
@@ -4,7 +4,7 @@ mkdir -p ASCII_GRID_1part/lines
 LINE=1;
 while read -r x;do
     NAME=$( printf "%06i" $LINE )
-    echo "$x" > ASCII_GRID_1part/$NAME.txt
+    echo "$x" > ASCII_GRID_1part/lines/$NAME.txt
     LINE=$(( LINE + 1 ))
 done < ASCII_GRID_1part/dhm25_grid_raster.asc
 
